caec8f6428faef2f0b6f942623d9bbc8872b8fbe
wiaam hilal <weaam224112@gmail.com>
2025-03-18 09:52:14 -0700

    all goodfdffddfddfdffdfdfddfggffdfdfdfgdgfdfgdgddfggdgddgdrdfggdddgdg

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ setInterval(() => {
     .on("error", (err) => {
       console.error(`Error: ${err.message}`);
     });
-}, 600000); // Ping every 5 minutes
+}, 1600000); // Ping every 5 minutes
 
 // translate the website
 const translate = require("google-translate-api-x");
