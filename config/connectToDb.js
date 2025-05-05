ef2362325e371bf1eaabed7c9430f29b5273dbbe
wiaam hilal <weaam224112@gmail.com>
2025-05-05 03:32:54 -0700

    kjkgdf

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -1,12 +1,29 @@
+// const mongoose = require("mongoose");
+// const url =
+//   "mongodb+srv://wiaambusiness28:3aWtJeOh3kxhmyJu@cluster0.zqolu1d.mongodb.net/blog";
+// module.exports = async () => {
+//   try {
+//     mongoose.connect(url);
+//     console.log("connected to database ^_^");
+//   } catch (error) {
+//     console.log("conection is faild", error);
+//   }
+// };
+
 const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://wiaambusiness28:3aWtJeOh3kxhmyJu@cluster0.zqolu1d.mongodb.net/blog?retryWrites=true&w=majority";
 
 module.exports = async () => {
   try {
-    mongoose.connect(
-      "mongodb+srv://wiaambusiness28:3aWtJeOh3kxhmyJu@cluster0.zqolu1d.mongodb.net/blog"
-    );
-    console.log("connected to database ^_^");
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // زيادة المهلة لتفادي timeout
+    });
+    console.log("✅ Connected to MongoDB");
   } catch (error) {
-    console.log("conection is faild", error);
+    console.error("❌ Connection to MongoDB failed:", error.message);
   }
 };
--- config/requistsSender.js
+++ config/requistsSender.js
@@ -10,15 +10,15 @@
 //     });
 // }, 300000); // Ping every 5 minutes
 
-const axios = require("axios");
+// const axios = require("axios");
 
-const url = "https://back-end-mern-9wjc.onrender.com"; // رابط موقعك
+// const url = "https://back-end-mern-1-c5va.onrender.com"; // رابط موقعك
 
-setInterval(async () => {
-  try {
-    const response = await axios.get(url);
-    console.log(`Pinged ${url}, Status Code: ${response.status}`);
-  } catch (error) {
-    console.error(`Error pinging ${url}:`, error.message);
-  }
-}, 300000); // 300000 ميلي ثانية (5 دقائق)
+// setInterval(async () => {
+//   try {
+//     const response = await axios.get(url);
+//     console.log(`Pinged ${url}, Status Code: ${response.status}`);
+//   } catch (error) {
+//     console.error(`Error pinging ${url}:`, error.message);
+//   }
+// }, 300000); // 300000 ميلي ثانية (5 دقائق)
